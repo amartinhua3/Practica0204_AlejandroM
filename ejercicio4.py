8b62c5e1bc3fc2665f75b701cab4175c8aefb73e
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-11-09 16:23:06 +0100

    ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+d = {}      
+n = input('Escriba su nombre: ')
+d['nombre'] = n
+print(d)
+e = int(input('Introduzca su edad: '))
+d['edad'] = e
+print(d)
+s = input('Introduzca su género: ')
+d['género'] = s
+print(d)
+t = int(input('Introduzca su número de teléfono: '))
+d['teléfono'] = t
+print(d)
+c = input('Escriba su corre electrónico: ')
+d['correo electrónico'] = c
+print(d)
