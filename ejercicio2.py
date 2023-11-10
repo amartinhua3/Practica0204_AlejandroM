53debd76dabe053e70d897a41ee6adae031c1f74
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-11-10 19:42:50 +0100

    ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+n = {input('Escriba su nombre: ')}
+
+e = {input('Escriba su edad: ')}
+
+d = {input('Escriba su dirección: ')}
+
+t = {input('Escriba su teléfono: ')}
+
+print(n['nombre'], 'tiene', e['edad'],'años, vive en', d['dirección'], 'y su número de teléfono es', t['teléfono'])
